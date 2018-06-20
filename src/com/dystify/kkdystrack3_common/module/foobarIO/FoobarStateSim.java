17d1cfaa3f06d0b7f869784b33b10d6d5e7b17df
BrianDuemmer <bduemmer1@gmail.com>
2018-06-19 20:46:07 -0400

    Started skeleton for foobar module

---
+++ src/com/dystify/kkdystrack3_common/module/foobarIO/FoobarStateSim.java
@@ -0,0 +1,6 @@
+package com.dystify.kkdystrack3_common.module.foobarIO;
+
+class FoobarStateSim
+{
+  
+}
--- src/com/dystify/kkdystrack3_common/module/foobarIO/package-info.java
+++ src/com/dystify/kkdystrack3_common/module/foobarIO/package-info.java
@@ -1,4 +1,17 @@
 /**
+ * 
+ * This module's job is to provide an interface for the Dystrack system to
+ * Foobar2000, the music player. This interface is through the command line.
+ * Certain actions, such as playback control, and adding songs to the playback
+ * queue. However, there isn't any sort of a feedback mechanism, so to overcome
+ * this limitation, the module keeps a daemon thread running to keep track of
+ * foobar's state.
+ * <p/>
+ * As was discovered in the original Core, losing sync between the simulator and
+ * foobar itself can have disastrous consequences of obscure issues that I am
+ * sure we are all already all too familiar with. As such, great care was taken
+ * to keep the simulation thread running as accurately as possible.
+ * 
  * @author Duemmer
  *
  */
